c608ca2f95e7e2cf77a77b3eb5c0f2a321557ee0
Cole Padden <colton.padden@fastmail.com>
2021-08-13 11:53:36 -0400

    implement example jobhero resume using tailwindcss

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,100 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+  <!-- Inspired by https://www.jobhero.com/resume/examples/arts/illustrator -->
+
+  <article class="lg:mx-64">
+    <div class="bg-yellow-100 h-24" />
+    <div class="bg-white h-2" />
+    <div class="border border-black p-3 mx-16 -mt-16 text-center">
+      <p class="text-5xl italic">Joaquin Barnett</p>
+    </div>
+    <div class="bg-gray-50 h-full p-4">
+      <p class="text-center py-2">
+        joaquinbarnett@gmail.com | 123 456 7890 | San Francisco, CA 94016
+      </p>
+      <div class="grid grid-flow-col gap-8">
+        <div class="col-span-3">
+          <div>
+            <h2 class="text-yellow-600 uppercase italic py-4">
+              Professional Summary
+            </h2>
+            <div>
+              For years parents have espoused the health benefits of eating
+              garlic bread with cheese to their children, with the food earning
+              such an iconic status in our culture that kids will often dress up
+              as warm, cheesy loaf for Halloween.
+            </div>
+          </div>
+          <div>
+            <h2 class="text-yellow-600 uppercase italic py-4">Work History</h2>
+            <div class="pb-4">
+              <h3><strong>Acme Inc.</strong>, San Francisco, CA</h3>
+              <h4><em>Artist</em></h4>
+              <ul class="list-disc px-4">
+                <li>
+                  Designed visual models meeting both aesthetic and technical
+                  criteria..
+                </li>
+                <li>
+                  Managed all art and office supply needs for art department.
+                </li>
+                <li>
+                  Utilized oil paints, acrylics and charcoal to design mixed
+                  media, drawings and painting pieces.
+                </li>
+                <li>
+                  Facilitated any requests regarding environment and level art
+                  creation process.
+                </li>
+                <li>Updated files in department asset management system.</li>
+              </ul>
+            </div>
+            <div class="pb-4">
+              <h3><strong>Acme Arts</strong>, San Francisco, CA</h3>
+              <h4><em>Artist</em></h4>
+              <ul class="list-disc px-4">
+                <li>
+                  Made use of materials, such as ink, watercolors, charcoal,
+                  oil, computer software, pints, and pencils, to complete unique
+                  piece for customers.
+                </li>
+                <li>
+                  Researched industry trends and evaluated competitive products.
+                </li>
+                <li>
+                  Fostered highly communicative, collaborative team culture on
+                  all visual product projects
+                </li>
+                <li>
+                  Facilitated any requests regarding environment and level art
+                  creation process.
+                </li>
+                <li>Updated files in department asset management system.</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <div class="col-span-2">
+          <h2 class="text-yellow-600 uppercase italic py-4">Skills</h2>
+          <ul class="list-disc px-4">
+            <li>Corporate Identity Branding</li>
+            <li>Video Game Product Development</li>
+            <li>Texture Painting</li>
+            <li>Visual Communication Expertise</li>
+            <li>Composition</li>
+          </ul>
+          <h2 class="text-yellow-600 uppercase italic py-4">Education</h2>
+          <div><strong>Palo Alto University</strong></div>
+          <div>Palo Alto, CA</div>
+          <div>05/2016</div>
+          <div><strong>Bachelor's in Arts</strong></div>
+        </div>
+      </div>
+    </div>
+  </article>
 </template>
 
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-
-// This starter template is using Vue 3 experimental <script setup> SFCs
-// Check out https://github.com/vuejs/rfcs/blob/master/active-rfcs/0040-script-setup.md
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
